7d92c5ff1d7ddc9eb6f1c2e562f95d353c678f40
Jingyuan Chen <79951870+jchen3031@users.noreply.github.com>
2022-05-22 20:57:27 -0400

    Update QAgent.py

--- QAgent.py
+++ QAgent.py
@@ -52,7 +52,7 @@ class QLearningAgent():
             newstate = pop[0]
         #print('finish update')
     def simulate(self):
-        b = board.board(size = 15, rule = 5)
+        b = board.board(size = 3, rule = 3)
         state = b.getstate()
         #print(state)
         stone = 1
